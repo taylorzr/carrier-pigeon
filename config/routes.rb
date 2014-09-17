97d3750618e1ec9ebdb9fd6123fd0b6a585a6406
Theo Skolnik <theodoraskolnik@gmail.com>
2014-09-17 15:58:16 -0500

    Added login and sign up, missing logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :users
+  # match '/signup' => 'users#new'
+
   resources :deliveries
+
+  get '/login'  => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
 
 end
