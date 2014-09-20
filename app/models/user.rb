02d850f410b8d2f6d334907086c88159ea330e44
Clayton Jordan <clayjchicago@gmail.com>
2014-09-20 18:10:07 -0500

    everything is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,55 +40,46 @@ class User < ActiveRecord::Base
 
   def requested_sends
 
-    self.sent_deliveries.where('departure_date > ?', Date.today).where(carrier_id: nil)
-    #no carrier_id, date is in future
+    self.sent_deliveries.where(carrier_id: nil).where.not(recipient_id: nil)
+    #no carrier_id, date does not apply
   end
 
   def pending_sends
-     self.sent_deliveries.where('departure_date > ?', Date.today).where(carrier_id: nil)
+     self.sent_deliveries.where('departure_date > ?', Date.today).where.not(carrier_id: nil)
 
     #yes carrier_id, date is in future
   end
 
   def completed_sends
-    #yes carrier_id, date is in future
+    self.sent_deliveries.where('departure_date < ?', Date.today).where.not(carrier_id: nil)
+
+    #yes carrier_id, date is in the past
   end
 
   def incomplete_sends
+    self.sent_deliveries.where('departure_date < ?', Date.today).where(carrier_id: nil)
     #no carrier_id, yes recipient_id, date is in past
   end
 
   def requested_carries
+    self.carried_deliveries.where('departure_date > ?', Date.today).where(sender_id: nil).where(recipient_id: nil)
     #no sender_id, no recipient_id, date is in future
   end
 
   def pending_carries
+    self.carried_deliveries.where('departure_date > ?', Date.today).where.not(sender_id: nil).where.not(recipient_id: nil)
     #yes sender_id, yes recipient_id, date is in future
   end
 
   def completed_carries
+    self.carried_deliveries.where('departure_date < ?', Date.today).where.not(sender_id: nil).where.not(recipient_id: nil)
     #yes sender_id, yes recipient_id, date is in past
   end
 
   def incomplete_carries
-    #yes sender_id, yes recipient_id, date is in past
+    self.carried_deliveries.where('departure_date < ?', Date.today).where(sender_id: nil).where(recipient_id: nil)
+    #no sender_id, no recipient_id, date is in past
 
   end
-
-  # def future_sent_deliveries
-  #   self.sent_deliveries.where(carrier_id: nil)
-  # end
-
-  # def past_sent_deliveries
-  #   self.sent_deliveries.where('departure_date < ?', Date.today).where.not(carrier_id: nil)
-  # end
-
-  # def future_carried_deliveries
-  #   self.carried_deliveries.where('departure_date > ?', Date.today).where(sender_id: nil)
-  # end
-
-  # def past_carried_deliveries
-  #   self.carried_deliveries.where('departure_date < ?', Date.today).where.not(sender_id: nil)
-  # end
 
 end
