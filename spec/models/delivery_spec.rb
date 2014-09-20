9382d1e23aa5b13de888807bb98a99cc56fff91c
Zach Taylor <taylorzr@gmail.com>
2014-09-20 13:00:23 -0500

    Adds complete delivery model tests

--- spec/models/delivery_spec.rb
+++ spec/models/delivery_spec.rb
@@ -4,19 +4,53 @@ describe 'Delivery' do
   before(:each) do
     @user1 = User.create(name: "Clayton", username: "Tester", email: "me@example", password_digest: "password", city: "Chicago")
     @user2 = User.create(name: "Mrs. Adolfo Fadel", username: "rosalee_renner", email: "jaron.mohr@blicklakin.info", password_digest: "password", city: "East Kaden")
-    @delivery = Delivery.create(carrier_id: 1, sender_id: 2, recipient_id: 1, package_size: "Large", from_city: "Chicago", to_city: "Austin", price: 20, departure_date: Date.today, arrival_date: Date.tomorrow)
+    @delivery = Delivery.create(
+      carrier_id: 1, 
+      sender_id: 2, 
+      recipient_id: 1, 
+      package_size: "Large", 
+      from_city: "Chicago", 
+      to_city: "Austin", 
+      price: 20, 
+      departure_date: Date.today, 
+      arrival_date: Date.tomorrow
+    )
     @recipient = Recipient.create(user_id: 2, name: "Theo", email: "test")
   end
 
-  it "should show that the delivery's carrier is 'Clayton'" do
+  it "should show that the carrier is 'Clayton'" do
     expect(@delivery.carrier.name).to eq('Clayton')
   end
 
-  it "should show that the delivery's sender is 'Mrs. Adolfo Fadel'" do
+  it "should show that the sender is 'Mrs. Adolfo Fadel'" do
     expect(@delivery.sender.name).to eq('Mrs. Adolfo Fadel')
   end
 
-  it "should show that the delivery's recipient is 'Theo'" do
+  it "should show that the recipient is 'Theo'" do
     expect(@delivery.recipient.name).to eq('Theo')
+  end
+
+  it "should have a package size of large" do
+    expect(@delivery.package_size).to eq("Large")
+  end
+
+  it "should have a from_city of Chicago" do
+    expect(@delivery.from_city).to eq("Chicago")
+  end
+
+  it "should have a to_city of Austin" do
+    expect(@delivery.to_city).to eq("Austin")
+  end
+
+  it "should have a price of 20" do
+    expect(@delivery.price).to eq(20)
+  end
+
+  it "should have a departure date of today" do
+    expect(@delivery.departure_date).to eq(Date.today)
+  end
+
+  it "should have a arrival date tomorrow" do
+    expect(@delivery.arrival_date).to eq(Date.tomorrow)
   end
 end
